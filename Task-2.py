19d6d8e9122915dfaf8f3d7f757999cb185370d4
Sahil Prasad <sahilprasad312@gmail.com>
2026-02-26 20:13:41 +0530

    Task-2.py

---
+++ Task-2.py
@@ -0,0 +1,12 @@
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+print("\nResults:")
+print("Square root:", square_root)
+print("Natural logarithm (log base e):", natural_log)
+print("Sine (in radians):", sine_value)
